b96a6ceb98090881e3740cbda272c9f9b3c8bb75
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2022-11-06 14:45:10 +0000

    css alterations

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var weatherSearch = document.querySelector("weather");
 var searchButton = document.getElementById("search-button");
 
 function getApi() {
-  // replace `octocat` with anyone else's GitHub username
   var requestUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76119befad1d3879033ce0396274f555";
 
@@ -19,4 +18,34 @@ function getApi() {
     });
 }
 
-fetchButton.addEventListener("click", getApi);
+searchButton.addEventListener("click", getApi);
+
+var requestUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76119befad1d3879033ce0396274f555";
+$.ajax({
+  url: requestUrl,
+  method: "GET",
+}).then(function (response) {
+  console.log("AJAX Response \n-------------");
+  console.log(response);
+});
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("Fetch Response \n-------------");
+    console.log(data);
+  });
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    console.log("XMLHttpRequest Response \n-------------");
+
+    console.log(xhr.response);
+  }
+};
+
+xhr.open("GET", requestUrl);
+xhr.send();
