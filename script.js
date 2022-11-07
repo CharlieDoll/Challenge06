004d7c1a7bfb33ebb12b0671f8bf8629ba206e0a
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2022-11-07 19:01:57 +0000

    api javascript code

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
 var weatherSearch = document.querySelector("weather");
 var searchButton = document.getElementById("search-button");
+var inputEl = document.getElementById("input-city");
 
 function getApi() {
+  var cityName = inputEl.value;
+
   var requestUrl =
-    "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76119befad1d3879033ce0396274f555";
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&APPID=76119befad1d3879033ce0396274f555";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement("li");
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
+      console.log(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log(lat, lon);
+      fiveDaysRequest(lat, lon);
+    });
+}
+
+function fiveDaysRequest(lat, lon) {
+  var fiveDaysUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&appid=76119befad1d3879033ce0396274f555";
+  fetch(fiveDaysUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (fivedays) {
+      console.log(fivedays);
     });
 }
 
 searchButton.addEventListener("click", getApi);
-
-var requestUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76119befad1d3879033ce0396274f555";
-$.ajax({
-  url: requestUrl,
-  method: "GET",
-}).then(function (response) {
-  console.log("AJAX Response \n-------------");
-  console.log(response);
-});
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log("Fetch Response \n-------------");
-    console.log(data);
-  });
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    console.log("XMLHttpRequest Response \n-------------");
-
-    console.log(xhr.response);
-  }
-};
-
-xhr.open("GET", requestUrl);
-xhr.send();
