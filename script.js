b65b70d83df4d2be37de23eb5e2ca72f06548404
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-02-28 19:06:24 +0000

    update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var weatherSearch = document.querySelector("weather");
 var searchButton = document.getElementById("search-button");
 var inputEl = document.getElementById("input-city");
+var fiveDayForcastEl = document.getElementById("fiveDayForcast");
+var weatherCont = document.getElementById("weatherCont");
+var allSearch = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
 function getApi() {
   var cityName = inputEl.value;
-
+  allSearch.push(cityName);
+  localStorage.setItem("searchHistory", JSON.stringify(allSearch));
   var requestUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
@@ -16,6 +20,14 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
+      const date = new Date().toLocaleDateString();
+      weatherCont.innerHTML = `
+      ${data.name} 
+      ${date}
+     Humidity: ${data.main.humidity} 
+     Temp: ${data.main.temp}
+     Wind: ${data.wind.speed}
+      `;
       var lat = data.coord.lat;
       var lon = data.coord.lon;
       console.log(lat, lon);
@@ -36,40 +48,44 @@ function fiveDaysRequest(lat, lon) {
     })
     .then(function (fiveDaysUrl) {
       console.log("testing second call", fiveDaysUrl);
+      // fiveDayForcastEl.innerHTML = "";
       const weatherList = fiveDaysUrl.list;
       const weatherDisplaycard = document.querySelectorAll(".card");
       let j = 0;
       for (let i = 0; i < weatherList.length; i += 8) {
         console.log(weatherDisplaycard[j]);
         console.log("index of j", j);
-
+        const displayDateEl = document.createElement("header");
         const mainWeatherElement = document.createElement("h2");
         const weatherDescriptionElement = document.createElement("p");
         const displayCityEl = document.createElement("div");
         const displayWindEl = document.createElement("div");
         const displayTempEl = document.createElement("div");
         const displayHumidityEl = document.createElement("div");
-        const displayFiveDaysEl = document.createElement("div");
+        // const displayFiveDaysEl = document.createElement("div");
 
         mainWeatherElement.textcontent =
           weatherList[i].weather[0].mainWeatherElement;
         weatherDescriptionElement.textContent =
           weatherList[i].weather[0].description;
 
-        displayCityEl.innerHTML = weatherList[i].main.city;
-        displayHumidityEl.innerHTML = weatherList[i].main.humidity;
-        displayWindEl.innerHTML = weatherList[i].main.wind;
-        displayTempEl.innerHTML = weatherList[i].main.temp;
-        displayFiveDaysEl.innerHTML = weatherList[i].main.FiveDays;
+        displayDateEl.innerHTML = weatherList[i].dt_txt;
+        displayCityEl.innerHTML = fiveDaysUrl.city.name;
+        displayHumidityEl.innerHTML =
+          "Humidity: " + weatherList[i].main.humidity;
+        displayWindEl.innerHTML = "Wind Speed: " + weatherList[i].wind.speed;
+        displayTempEl.innerHTML = "Temperature: " + weatherList[i].main.temp;
+        // displayFiveDaysEl.innerHTML = weatherList[i].main.FiveDays;
 
         weatherDisplaycard[j].append(
+          displayDateEl,
           mainWeatherElement,
           weatherDescriptionElement,
           displayCityEl,
           displayWindEl,
           displayTempEl,
-          displayHumidityEl,
-          displayFiveDaysEl
+          displayHumidityEl
+          // displayFiveDaysEl
         );
 
         j++;
